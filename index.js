7d59dfb3f12b22c73567fe539f2d4d1d87bd2f4b
Tiest van Gool <tiestvangool@icloud.com>
2020-04-30 14:49:20 -0600

    Higher-Order Functions Examined

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-//The Sum of a Range
-function range(start, end) {
-  let array = [];
-  for(i = start; i <= end; i += 1) {
-    array.push(i);
+var SCRIPTS = [
+  {
+    name: "Adlam",
+    ranges: [[125184, 125259], [125264, 125274], [125278, 125280]],
+    direction: "rtl",
+    year: 1987,
+    living: true,
+    link: "https://en.wikipedia.org/wiki/Fula_alphabets#Adlam_alphabet"
+  },
+  {
+    name: "Caucasian Albanian",
+    ranges: [[66864, 66916], [66927, 66928]],
+    direction: "ltr",
+    year: 420,
+    living: false,
+    link: "https://en.wikipedia.org/wiki/Caucasian_Albanian_alphabet"
+  },
+  {
+    name: "Ahom",
+    ranges: [[71424, 71450], [71453, 71468], [71472, 71488]],
+    direction: "ltr",
+    year: 1250,
+    living: false,
+    link: "https://en.wikipedia.org/wiki/Ahom_alphabet"
   }
-  return(array);
+];
+
+// Filtering Arrays
+function z_filter(array, test) {
+  let passed = [];
+  for (let element of array) {
+    if (test(element)) {
+      passed.push(element);
+    }
+  }
+  return(passed);
 }
 
-function sum(range) {
-  let total = 0;
-  for (value of range) {
-    total += value;
+//Output custom function and standard function
+console.log(z_filter(SCRIPTS, script => script.living));
+console.log(SCRIPTS.filter(s => s.direction == "ltr"));
+
+//Summarize with reduce
+function z_reduce(array, combine, start) {
+  let current = start;
+  for (let element of array) {
+    current = combine(current, element);
   }
-  return(total);
+  return(current);
 }
 
-//Output Create
-console.log(range(1, 10));
-console.log(sum(range(1, 10)));
+//Output custom function and standard function
+console.log(z_reduce([1,2,3,4,5], (a, b) => a + b, 0));
+console.log([1, 2, 3, 4].reduce((a, b) => a + b)); 
