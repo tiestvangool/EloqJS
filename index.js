1de3f3c409babc0f32c275c01f6c0382a2794bcb
Tiest van Gool <tiestvangool@icloud.com>
2020-04-30 11:45:46 -0600

    Created range function from values

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-//Tree
-for (tree = "#"; tree.length <= 7; tree += "#") {
-  console.log(tree);
+//The Sum of a Range
+function range(start, end) {
+  let array = [];
+  for(i = start; i <= end; i += 1) {
+    array.push(i);
+  }
+  return(array);
 }
+
+
+console.log(range(1, 10));
